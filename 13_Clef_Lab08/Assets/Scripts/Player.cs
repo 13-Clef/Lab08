9da9b006fa95ddac4842bac2a428ac7c877fa1fa
13-Clef <rocketmail72002@gmail.com>
2022-10-26 12:48:57 +0800

    Update Unity Lab08
    Completed Lab08

---
+++ 13_Clef_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public static int score;
+    public GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+
+        scoreText.GetComponent<Text>().text = "Score: " + score;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
